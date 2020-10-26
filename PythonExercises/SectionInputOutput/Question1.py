c8f760d2673e62af86514334e36121997ce1c3fa
DanielDavtyan <danieldavtyan99@gmail.com>
2020-10-26 05:44:01 +0400

    Added my code of the bot(not full)

---
+++ PythonExercises/SectionInputOutput/Question1.py
@@ -0,0 +1,11 @@
+#Accept two numbers from the user and calculate multiplication
+
+
+
+def multiplication():
+    x = int(input("please enter the first number"))
+    y = int(input("Enter the second number"))
+    return x*y
+
+
+print(multiplication())
